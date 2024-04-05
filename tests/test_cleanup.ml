18a9ec364f2dca1271c4acee2cc4d5bd43989e22
Rizo I <rizo@odis.io>
2024-04-05 21:01:38 +0100

    test: add nested cleanup tests

--- tests/test_cleanup.ml
+++ tests/test_cleanup.ml
@@ -43,9 +43,92 @@ let test_leaky_1 () =
         [ text "Increment" ];
     ]
 
+let test_nested_1 () =
+  let n1 = Signal.make 100 in
+  let n2 = Signal.make 200 in
+  let n3 = Signal.make 300 in
+  let open Html in
+  div
+    [ id "main" ]
+    [
+      show ~label:"n1"
+        (fun n1 ->
+          div []
+            [
+              text ("n1: " ^ string_of_int n1);
+              show ~label:"n2"
+                (fun n2 ->
+                  div []
+                    [
+                      text ("n2: " ^ string_of_int n2);
+                      br [];
+                      show ~label:"n3"
+                        (fun n3 -> text ("n3: " ^ string_of_int n3))
+                        n3;
+                    ]
+                )
+                n2;
+            ]
+        )
+        n1;
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n1) ]
+        [ text "incr n1" ];
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n2) ]
+        [ text "incr n2" ];
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n3) ]
+        [ text "incr n3" ];
+    ]
+
+let test_nested_2 () =
+  let n1 = Signal.make 100 in
+  let n2 = Signal.make 200 in
+  let n3 = Signal.make 300 in
+  let open Html in
+  div
+    [ id "main" ]
+    [
+      show ~label:"n1"
+        (fun n1 ->
+          show ~label:"n2"
+            (fun n2 ->
+              show ~label:"n3"
+                (fun n3 ->
+                  text
+                    (String.concat ", " (List.map string_of_int [ n1; n2; n3 ]))
+                )
+                n3
+            )
+            n2
+        )
+        n1;
+      br [];
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n1) ]
+        [ text "incr n1" ];
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n2) ]
+        [ text "incr n2" ];
+      button
+        [ on_click (fun () -> Signal.update (( + ) 1) n3) ]
+        [ text "incr n3" ];
+    ]
+
 let main () =
   let open Html in
-  div [] [ h2 [] [ text "test_leaky_1" ]; test_leaky_1 () ]
+  div []
+    [
+      h2 [] [ text "test_leaky_1" ];
+      test_leaky_1 ();
+      br [];
+      h2 [] [ text "test_nested_1" ];
+      test_nested_1 ();
+      br [];
+      h2 [] [ text "test_nested_2" ];
+      test_nested_2 ();
+    ]
 
 let () =
   match Stdweb.Dom.Document.get_element_by_id "root" with
